6b35f8d18a835415c5c2310a0d2224ca17a14d51
Blora616 <Blora616@gmail.com>
2019-10-16 10:57:32 -0400

    finish

--- usps-shipping-service/src/main/java/com/cognizant/uspsshippingservice/controller/UspsShippingServiceController.java
+++ usps-shipping-service/src/main/java/com/cognizant/uspsshippingservice/controller/UspsShippingServiceController.java
@@ -1,8 +1,33 @@
 package com.cognizant.uspsshippingservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.cognizant.uspsshippingservice.dao.ShipmentRepository;
+import com.cognizant.uspsshippingservice.dto.Shipment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
+@RequestMapping(value = "/shipment")
 public class UspsShippingServiceController {
+    @Autowired
+    ShipmentRepository shipmentRepository;
 
+    @PostMapping(value = "/addshipment")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Shipment createShipment(@RequestBody @Valid Shipment shipment) {
+        return shipmentRepository.save(shipment);
+    }
+
+    @GetMapping(value = "/{trackingNumber}")
+    @ResponseStatus(HttpStatus.OK)
+    public Shipment getShipmentByTrackingNumber(@PathVariable int trackingNumber) {
+        Shipment shipment = shipmentRepository.findByTrackingNumber(trackingNumber);
+        if (shipment == null) {
+            throw new IllegalArgumentException("Not found.");
+        }
+        return shipment;
+    }
 }

74c0fce6669f25fdfdd509eca2a99b980ec2e1b8
Blora616 <Blora616@gmail.com>
2019-10-16 10:06:43 -0400

    initi

---
+++ usps-shipping-service/src/main/java/com/cognizant/uspsshippingservice/UspsShippingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.uspsshippingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UspsShippingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UspsShippingServiceApplication.class, args);
+	}
+
+}
